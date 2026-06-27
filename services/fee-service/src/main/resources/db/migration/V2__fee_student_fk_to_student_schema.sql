d139965de1a29fa4a171084a46a61ae208f65b44
Shubham Bagrodia <shubhambagrodia2609@gmail.com>
2026-06-27 10:31:49 +0530

    fix: remove cross-service migration foreign keys

--- services/fee-service/src/main/resources/db/migration/V2__fee_student_fk_to_student_schema.sql
+++ services/fee-service/src/main/resources/db/migration/V2__fee_student_fk_to_student_schema.sql
@@ -1,11 +1,5 @@
 ALTER TABLE fee_assignments DROP CONSTRAINT IF EXISTS fee_assignments_student_id_fkey;
 ALTER TABLE fee_assignments DROP CONSTRAINT IF EXISTS fk_fee_assignment_student;
-ALTER TABLE fee_assignments
-    ADD CONSTRAINT fk_fee_assignment_student
-    FOREIGN KEY (student_id) REFERENCES student.students(id);
 
 ALTER TABLE payment_records DROP CONSTRAINT IF EXISTS payment_records_student_id_fkey;
 ALTER TABLE payment_records DROP CONSTRAINT IF EXISTS fk_payment_student;
-ALTER TABLE payment_records
-    ADD CONSTRAINT fk_payment_student
-    FOREIGN KEY (student_id) REFERENCES student.students(id);
